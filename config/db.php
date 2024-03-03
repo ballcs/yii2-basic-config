5549867ffd621f65c17e0e0ed8f529129df56c31
Wichukorn Dandecha <wichukorn.dandecha@gmail.com>
2024-03-03 11:28:41 +0700

    Hello World

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=10.250.1.99;dbname=yii2-testing',
+    'username' => 'yii2-testing',
+    'password' => 'gmw2ymf-FKF4qhf9taz',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
